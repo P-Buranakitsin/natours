eb2d1be55ccd629be499c430acd55eeee28bceec
Pavaruth Buranakitsin <pavaruth.buranakitsin@gmail.com>
2022-07-03 21:26:54 +0700

    fix bug

--- public/js/index.js
+++ public/js/index.js
@@ -73,10 +73,10 @@ if (bookBtn) {
     const { tourId } = e.target.dataset
     bookTour(tourId)
   })
+}
 
-  const alertMessage = document.querySelector('body').dataset.alert;
+const alertMessage = document.querySelector('body').dataset.alert;
   if (alertMessage) {
     showAlert('success', alertMessage, 20)
-  }
 }
 
